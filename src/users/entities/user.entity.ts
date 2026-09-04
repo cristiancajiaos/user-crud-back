8ea60f491cd5991c92a76fbcecfa2aa6034ac041
Cristian Cajiao <cristiancajiaos@outlook.com>
2026-09-04 12:54:05 -0400

    Creada composición de la entidad User

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,18 @@
-export class User {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity({name: 'users'})
+export class User {
+
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column('text', {
+    unique: true
+  })
+  name: string;
+
+  @Column('text', {
+    unique: true
+  })
+  email: string;
+}
