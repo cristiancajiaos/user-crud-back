2782a6b3469f2aa70ed1f067ffba5be2a7d2a410
Cristian Cajiao <cristiancajiaos@outlook.com>
2026-09-04 13:04:12 -0400

    Definidos criterios de validación para los campos en el createUserDto

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,12 @@
-export class CreateUserDto {}
+import { IsEmail, IsString, MinLength } from "class-validator";
+
+export class CreateUserDto {
+
+  @IsString()
+  @MinLength(1)
+  name: string;
+
+  @IsString()
+  @IsEmail()
+  email: string;
+}
